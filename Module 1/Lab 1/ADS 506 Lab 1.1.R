bffca8ae3310a52d25299efe272b1bb01732cd8a
zfreitas <Zachariah.Freitas@viasat.com>
2022-10-29 12:28:11 -0700

    save

--- Module 1/Lab 1/ADS 506 Lab 1.1.R
+++ Module 1/Lab 1/ADS 506 Lab 1.1.R
@@ -1,8 +1,9 @@
 library(tidyverse)
+
 set.seed(506)
 
 # get our data
-dgf_sales <- read_csv("Data/DGF Sales FY21-22.csv")
+dgf_sales <- read_csv("Lab 1/DGF Sales FY21-22.csv")
 
 View(dgf_sales)
 
@@ -14,19 +15,24 @@ dgf_sales$Date <- as.Date(dgf_sales$Date, format = "%m/%d/%Y")
 summary(dgf_sales)
 
 
+
+# Create Regular Intervals - Daily Totals
 dgf_daily <- dgf_sales %>%
   group_by(Date) %>%
   summarise(Total  = sum(Subtotal))
 
 
+# Calculate Summary Statistics
 summary(dgf_daily)
 
-
+# Plot the daily sales
 ggplot(dgf_daily, aes(Date, Total)) +
   geom_line() +
   theme_classic()
+# This data is not stationary
 
 
+# Create Regular Intervals - Monthly Totals
 dgf_monthly <- dgf_sales %>%
   group_by(Month = format(Date, "%Y-%m")) %>%
   summarise(Total = sum(Subtotal))
--- Module 1/Lab 2/ADS 506 Lab 1.2.R
+++ Module 1/Lab 2/ADS 506 Lab 1.2.R
@@ -1,9 +1,10 @@
 library(tidyverse)
-library(fpp2)
+library(fpp2) # Plot and Forecast Data
 set.seed(506)
 
 
-dgf_vol <- read_csv("Data/DGF Volume.csv", 
+# Get Data
+dgf_vol <- read_csv("Lab 2/DGF Volume.csv", 
                     col_types = cols(Date = col_date(format = "%m/%d/%Y")))
 
 
@@ -14,42 +15,63 @@ MyTS
 
 plot(MyTS)
 
+# GGplot - Time Serries Plot
 autoplot(MyTS) +
   labs(title = "DGF Sales Volume Over Time",
        x = "Time",
        y = "Sales volume") +
   theme_minimal()
 
-
+#############################################################
 # create 2 partitions
 # training is from July 2018 to December 2021
-# validaton is from Jan 2022 to May 2022
+# validation is from Jan 2022 to May 2022
 
 train <- window(MyTS, start = c(2018, 7), end = c(2021, 12))
-
+train
 validation <- window(MyTS, start = c(2022, 1))
+validation
 
+# Plot the Training and Validation Set
 autoplot(train) +
+  labs(title = "DGF Sales Volume Over Time",
+       x = "Time",
+       y = "Sales volume") +
+  theme_minimal() +
   autolayer(validation, color = "blue")
 
 
-# naive model
+# naive model -
+# This takes the very last value of the training set.
 n_model <- naive(train, h = 5, level = 95)
 
+
+# Plot the Training Model with the Naive Model.
 autoplot(train) +
+  labs(title = "DGF Sales Volume Over Time",
+       x = "Time",
+       y = "Sales volume") +
+  theme_minimal() +
   autolayer(n_model, color = "#006d2c", series = "Naive") +
   autolayer(validation, color = "blue", series = "Actual")
 
-# seasonal naive
+
+##################
+# Model: seasonal naive -
+# This takes a seasonal naive value.
 sn_model <- snaive(train, h = 5)
 
 autoplot(train) +
+  labs(title = "DGF Sales Volume Over Time",
+       x = "Time",
+       y = "Sales volume") +
+  theme_minimal() +
   autolayer(sn_model, color = "#006d2c", series = "Seasonal Naive") +
   autolayer(validation, color = "blue", series = "Actual")
 
 
-
-# season & trend
+##################
+# Model season & trend
 st_model <- tslm(train ~ trend + season)
 
 summary(st_model)
@@ -57,18 +79,26 @@ summary(st_model)
 st_forecast <- forecast(st_model, h = 5)
 
 autoplot(train) +
+  labs(title = "DGF Sales Volume Over Time",
+       x = "Time",
+       y = "Sales volume") +
+  theme_minimal() +
   autolayer(st_forecast, color = "#006d2c", series = "season trend") +
   autolayer(validation, color = "blue", series = "Actual")
 
 
+###############################
+# Measure the errors
 
 accuracy(st_forecast)
 accuracy(n_model)
 accuracy(sn_model)
 
 
+#MAE
 mean(abs(n_model$residuals), na.rm = T)
 
+# RMSE
 sqrt(mean(n_model$residuals^2, na.rm = T))
 
 
